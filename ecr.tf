78d37af91b17ba5b84c6f5771414202afa6ccf2c
lordkroft <4402po@gmail.com>
2021-12-13 12:09:12 +0300

    add ecrs for project

--- ecr.tf
+++ ecr.tf
@@ -1,3 +1,4 @@
+####THIS IS FOR PERSONAL USE###
 resource "aws_ecr_repository" "space-registry" {
   name = "space-registry"
   tags = {
@@ -33,6 +34,121 @@ EOF
 
 resource "aws_ecr_lifecycle_policy" "tagged" {
   repository = aws_ecr_repository.space-registry.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 10 images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["v"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 10
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+####THIS IS FOR PROJECT USE####
+resource "aws_ecr_repository" "frontend" {
+  name = "frontend"
+  tags = {
+    Name        = "frontend"
+    Environment = "${var.environment}"
+  }
+}
+
+
+resource "aws_ecr_lifecycle_policy" "untagged" {
+  repository = aws_ecr_repository.frontend.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 7 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 7
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_ecr_lifecycle_policy" "tagged" {
+  repository = aws_ecr_repository.frontend.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 10 images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["v"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 10
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_ecr_repository" "backend" {
+  name = "backend"
+  tags = {
+    Name        = "backend"
+    Environment = "${var.environment}"
+  }
+}
+
+
+resource "aws_ecr_lifecycle_policy" "untagged" {
+  repository = aws_ecr_repository.backend.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 7 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 7
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_ecr_lifecycle_policy" "tagged" {
+  repository = aws_ecr_repository.backend.name
 
   policy = <<EOF
 {
