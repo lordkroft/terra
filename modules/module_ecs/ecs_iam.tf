607329c472cef539ceea49228bd42df616a5747e
Vladimir Sinitsa <v.sinitsa@invento.by>
2021-12-16 23:23:05 +0300

    role

--- modules/module_ecs/ecs_iam.tf
+++ modules/module_ecs/ecs_iam.tf
@@ -5,8 +5,22 @@ resource "aws_iam_instance_profile" "ecs_ec2" {
 
 resource "aws_iam_role" "ecsIR" {
   name = "ecsIR"
-  assume_role_policy =  file("./modules/module_ecs/role-policy/ec2-role.json")
-  
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
   tags = {
     Name = "ecs_iam"
   }
@@ -14,8 +28,66 @@ resource "aws_iam_role" "ecsIR" {
 
 resource "aws_iam_role_policy" "amazonEC2ContainerServiceforEC2Role" {
   name   = "ecs_role_policy"
-  policy = file("./modules/module_ecs/role-policy/ecs-role-policy.json")
   role       = aws_iam_role.ecsIR.name
-  }
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecs:CreateCluster",
+                "ecs:DeregisterContainerInstance",
+                "ecs:DiscoverPollEndpoint",
+                "ecs:Poll",
+                "ecs:RegisterContainerInstance",
+                "ecs:StartTelemetrySession",
+                "ecs:UpdateContainerInstancesState",
+                "ecs:Submit*",
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents",
+                "s3:ListBucket",
+                "s3:PutObject",
+                "s3:GetObject"
+            ],
+            "Resource": "*"
+        
+        },
+    {
+        "Effect": "Allow",
+        "Action": [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents",
+            "logs:DescribeLogStreams"
+        ],
+        "Resource": [
+            "arn:aws:logs:*:*:*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iam:ListUserTags",
+                "iam:ListRoleTags",
+                "iam:TagUser",
+                "iam:TagRole",
+                "iam:UntagUser",
+                "iam:UntagRole"
+            ],
+            "Resource": [
+                
+                "arn:aws:iam::413752907951:role/ecsIR"
+            ]
+        }
+    ]
+}
+EOF
+}
 
 
